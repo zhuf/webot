13951c2e0a0dc9eaeb938129d427efbe966e9ae5
zhuf <zhuf@live.com>
2014-03-08 21:44:54 +0800

    add menu class

--- lib/menu.py
+++ lib/menu.py
@@ -1,7 +1,53 @@
 # -*- coding: utf-8 -*-
 
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+import json
+
 class Menu:
+    '''
+    {
+        "button":[
+        {  
+            "type":"click",
+            "name":"今日歌曲",
+            "key":"V1001_TODAY_MUSIC"
+        },
+        {
+            "name":"菜单",
+            "sub_button":[
+            {    
+                "type":"view",
+                "name":"搜索",
+                "url":"http://www.soso.com/"
+            },
+            {
+                "type":"view",
+                "name":"视频",
+                "url":"http://v.qq.com/"
+            }]
+        }]
+    }
+
+    m.addbutton({"type":"click","name":"今日歌曲","key":"V1001_TODAY_MUSIC"})
+    m.addbutton({"name":"菜单"},
+            [{"type":"view","name":"搜索","url":"http://www.soso.com/"},
+            {"type":"view","name":"视频","url":"http://v.qq.com/"}])
+
+    '''
     buttons = []
 
-    def addButton(self, button, subButtons):
+    def addbutton(self, button, subbuttons=[]):
+        if subbuttons != []:
+            button['sub_button'] = subbuttons
+        self.buttons.append(button)
+
+        return self.buttons
+
+    def tostring(self):
+        return json.dumps({'button': self.buttons})
+
+
 
